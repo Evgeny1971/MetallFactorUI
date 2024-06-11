4c391794a17aef1350b81066bcd264e886bb8ef7
Evgeny1971 <gen.vinnikov@gmail.com>
2024-06-11 08:47:46 +0300

    up

---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using MetallFactorUI.Model;
+
+namespace MetallFactorUI.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<WorkRequest> WorkRequests { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure your entity properties here if needed
+        }
+    }
+}
--- obj/Debug/net8.0/MetallFactorUI.AssemblyInfo.cs
+++ obj/Debug/net8.0/MetallFactorUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MetallFactorUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ab323b4a841a23f57a75551d3d42cf4d85b174")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8c5395f164d1e47ce7f3b1489b1560f418ab926")]
 [assembly: System.Reflection.AssemblyProductAttribute("MetallFactorUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MetallFactorUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
